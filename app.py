8b88c15502989a166f20b1253722b370cc3985ce
Mikaela Goldrich <mikaelagoldrich@Mikaelas-MacBook-Pro.local>
2018-02-22 21:25:23 -0500

    Import suggestions in app

--- app.py
+++ app.py
@@ -1,34 +1,19 @@
 #!flask/bin/python
 from flask import Flask, request
 from flask import jsonify
-import vocab
+import suggestions
 
 app = Flask(__name__)
 
-# tasks = [
-#     {
-#         'id': 1,
-#         'title': u'Buy groceries',
-#         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-#         'done': False
-#     },
-#     {
-#         'id': 2,
-#         'title': u'Learn Python',
-#         'description': u'Need to find a good Python tutorial on the web',
-#         'done': False
-#     }
-# ]
-#
-# @app.route('/todo/api/v1.0/tasks', methods=['GET'])
-# def get_tasks():
-#     return jsonify({'tasks': tasks})
-
 @app.route('/suggestions', methods=['GET'])
 def get_suggestions():
-    url = request.args.get('url')
-    print(url)
-    return jsonify({'synonyms': vocab.get_synonyms(url)})
-
+    customizer = request.args.get('customizer')
+    tlds = requests.args.get('tlds')
+    if (requests.args.get('lat') or requests.args.get('long')) is None:
+        return jsonify(suggestions.get(customizer, tlds))
+    else:
+        latlong = requests.args.get('lat') + requests.args.get('long')
+        print(latlong)
+        return jsonify(suggestions.get(customizer, tlds, latlong))
 if __name__ == '__main__':
     app.run(debug=True)
