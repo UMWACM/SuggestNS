7fcd2fd2b98beb6d766ab75749bfe7890ff1c7aa
Mikaela Goldrich <mikaelagoldrich@Mikaelas-MacBook-Pro.local>
2018-02-23 12:44:51 -0500

    Add intents

--- ask.py
+++ ask.py
@@ -1,46 +1,64 @@
 import logging
 from flask import Flask, render_template
-from flask_ask import Ask, statement, question
+from flask_ask import Ask, statement, question, session
+import suggestions
 
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
 # get address from alexa at some point
+count = 0
+
 
 
 @ask.launch
 def launch():
-    welcome_msg = render_template('welcome')
+    welcome_msg = render_template('greeting')
     return question(welcome_msg)
 
 
 @ask.intent('KeywordIntent',
-            default={'keyword': 'fishing'})
+            convert={'keyword' : str})
 def keyword(keyword):
-    key_msg = render_template('keyword')
+    session.attributes['keyword'] = keyword
+    key_msg = render_template('keyword', keyword=session.attributes['keyword'] )
     return question(key_msg)
 
-
 @ask.intent('TldIntent',
-            default={'choice': '.boats'})
-def tld(choice):
-    tld_msg = render_template('tld')
+            convert={'tld': str})
+def tld(tld):
+    # session.attributes['tld'] = choice
+    keyword = session.attributes['keyword']
+    url = keyword + " dot " + tld
+    tld_msg = render_template('tld', url=url)
+    session.attributes['original'] = url
+    session.attributes['tld'] = "." + tld
     return question(tld_msg)
 
 
-@ask.intent('OriginalIntent')
-def original():
-    return 0
+@ask.intent('SuggestFirstIntent')
+def first():
+    alexa_init(session.attributes['keyword'], session.attributes['tld'])
+    suggestion = alexa_suggestion(0)
+    global count
+    count++
+    url = session.attributes['url']
+    if False is in suggestion.values():
+        suggestion_msg = render_template(suggest_first_notavail, url=url)
+    else:
+        suggestion_msg = render_template(suggest_first_avail, url=url)
+    return question(suggestion_msg)
 
-
-@ask.intent('SuggestionIntent')
-def suggestion():
-    return 0
-
-
-@ask.intent('DoneIntent')
-def done():
-    return 0
+#
+# @ask.intent('SuggestionIntent')
+# def suggestion():
+#     return 0
+#
+#
+# @ask.intent('DoneIntent')
+# def done():
+#     return 0
 
 
 @ask.intent('AMAZON.StopIntent')
@@ -53,3 +71,7 @@ def stop():
 def cancel():
     bye_text = render_template('bye')
     return statement(bye_text)
+#
+
+if __name__ == '__main__':
+    app.run(debug=True)
