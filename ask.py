be549ac1efb94cfa1ca82799bcee2449651b9b3f
Mikaela Goldrich <mikaelagoldrich@Mikaelas-MacBook-Pro.local>
2018-02-23 13:41:22 -0500

    Finalize Alexa

--- ask.py
+++ ask.py
@@ -8,58 +8,29 @@ ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 # get address from alexa at some point
-count = 0
-
-
-
 @ask.launch
 def launch():
     welcome_msg = render_template('greeting')
     return question(welcome_msg)
 
 
-@ask.intent('KeywordIntent',
-            convert={'keyword' : str})
-def keyword(keyword):
-    session.attributes['keyword'] = keyword
-    key_msg = render_template('keyword', keyword=session.attributes['keyword'] )
+@ask.intent('KeywordIntent')
+def keyword():
+    key_msg = render_template('keyword')
     return question(key_msg)
 
-@ask.intent('TldIntent',
-            convert={'tld': str})
-def tld(tld):
-    # session.attributes['tld'] = choice
-    keyword = session.attributes['keyword']
-    url = keyword + " dot " + tld
-    tld_msg = render_template('tld', url=url)
-    session.attributes['original'] = url
-    session.attributes['tld'] = "." + tld
-    return question(tld_msg)
+# @ask.intent('TldIntent',
+#             convert={'tld': str})
+# def tld(tld):
+#     tld_msg = render_template('tld')
+#     return question(tld_msg)
+@ask.intent('YesIntent')
+def suggest():
+    return question(render_template('suggest1'))
 
-
-@ask.intent('SuggestFirstIntent')
-def first():
-    alexa_init(session.attributes['keyword'], session.attributes['tld'])
-    suggestion = alexa_suggestion(0)
-    global count
-    count++
-    url = session.attributes['url']
-    if False is in suggestion.values():
-        suggestion_msg = render_template(suggest_first_notavail, url=url)
-    else:
-        suggestion_msg = render_template(suggest_first_avail, url=url)
-    return question(suggestion_msg)
-
-#
-# @ask.intent('SuggestionIntent')
-# def suggestion():
-#     return 0
-#
-#
-# @ask.intent('DoneIntent')
-# def done():
-#     return 0
-
+@ask.intent('NoIntent')
+def no():
+    return statement(render_template('done'))
 
 @ask.intent('AMAZON.StopIntent')
 def stop():
